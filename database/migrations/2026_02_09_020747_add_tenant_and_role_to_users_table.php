6cd550387a918c837c4aca2282573986bd5aefd2
Youssef Ashraf <yusuufashraaf@icloud.com>
2026-02-09 04:09:26 +0200

    feat(auth): add tenant_id and role fields to users table

---
+++ database/migrations/2026_02_09_020747_add_tenant_and_role_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //      
+            $table->foreignId('tenant_id')->constrained()->cascadeOnDelete();
+            $table->enum('role', ['admin', 'member'])->default('member');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropForeign(['tenant_id']);
+            $table->dropColumn(['tenant_id', 'role']);
+
+        });
+    }
+};
