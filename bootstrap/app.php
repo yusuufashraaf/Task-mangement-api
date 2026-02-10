8da27348f3340e87dcafe9b8bb9c18610afa6c1d
Youssef Ashraf <yusuufashraaf@icloud.com>
2026-02-10 12:59:48 +0200

    feat(tenant): configure IdentifyTenant middleware alias

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+                $middleware->alias([
+            'tenant' => \App\Http\Middleware\IdentifyTenant::class,
+        ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
